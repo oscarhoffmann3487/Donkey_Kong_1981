8a4a21fbec009b70541e3919fdcd46e857d2317e
unknown <julja673@student.liu.se>
2022-02-21 20:24:12 +0100

    hoppas

--- src/Logic/Main.java
+++ src/Logic/Main.java
@@ -5,5 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("test");
 		System.out.println("test2");
+		System.out.println("test3");
+		System.out.println("hoppas det funkar nu");
 	}
 }
